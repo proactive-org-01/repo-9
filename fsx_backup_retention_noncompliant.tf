b16ba79f9ffc46e1b5482800cdd20975c2c66f58
Sneha0615 <113693337+Sneha0615@users.noreply.github.com>
2025-11-13 22:41:09 +0530

    fsxnon

---
+++ fsx_backup_retention_noncompliant.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_fsx_lustre_file_system" "fsx_noncompliant" {
+  storage_capacity               = 1200
+  subnet_ids                     = ["subnet-123456"]
+  automatic_backup_retention_days = 180
+}
